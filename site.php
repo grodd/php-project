0906aaff6573e646dbc2aac87925f387c69d9128
Manish Kashyap <groddmanish@gmail.com>
2020-02-07 09:06:48 +0530

    variables

--- site.php
+++ site.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>My first php page.</title>
 </head>
+
 <body>
-<?php
-echo("<h1>Manish's Site</h1>");
-?>
+  <?php
+  $characterName = "Tom";
+  $characterAge = 35;
+  echo ("There once was a man named $characterName <br>");
+  echo ("He was $characterAge years old <br>");
+  $characterName = "Mike";
+  echo ("He really liked the name $characterName <br>");
+  echo ("But didn't like being $characterAge <br>");
+  ?>
 </body>
+
 </html>
